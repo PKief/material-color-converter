93c1a5e5d1345b74c184b5b28c39afcc53dd6e80
PKief <philipp.kief@gmx.de>
2020-09-24 00:05:14 +0200

    Show multiple colors which are near to the input color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,62 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import * as chroma from 'chroma-js';
 import { materialColors } from './colors';
+
+interface ResultColor {
+  hex: string;
+  rgb: string;
+  hsl: string;
+  distance: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  animations: [],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'material-color-converter';
   colorForm: FormGroup;
-  resultColor: { hex: string; rgb: string; hsl: string };
+  resultColors: ResultColor[] = [];
 
   @ViewChild('container') container: ElementRef;
 
+  constructor(private cdr: ChangeDetectorRef) {}
+
   ngOnInit(): void {
+    const randomStartColor =
+      materialColors[Math.floor(Math.random() * materialColors.length)];
     this.colorForm = new FormGroup({
-      color: new FormControl('', [
+      color: new FormControl(randomStartColor, [
         Validators.required,
-        Validators.maxLength(6),
-        Validators.minLength(3),
         this.validateCssColor,
       ]),
     });
   }
 
+  ngAfterViewInit(): void {
+    this.convert();
+  }
+
+  changeColorPicker(event): void {
+    const color = event.srcElement.value;
+    this.colorForm.get('color').setValue(color);
+    this.convert();
+  }
+
   convert(): void {
+    if (this.colorForm.invalid) {
+      return;
+    }
     const { color } = this.colorForm.value;
     const distances = materialColors
       .map((matColor) => {
@@ -37,13 +67,24 @@ export class AppComponent implements OnInit {
       })
       .sort((a, b) => a.distance - b.distance);
 
-    const result = distances[0];
-    this.resultColor = {
-      hex: result.color,
-      rgb: chroma(result.color).css(),
-      hsl: chroma(result.color).css('hsl'),
-    };
-    this.container.nativeElement.style.backgroundColor = result.color;
+    this.resultColors = [];
+    distances.slice(0, 5).forEach((distance) => {
+      const suggestedColor = distance.color;
+      this.resultColors.push({
+        hex: suggestedColor,
+        rgb: chroma(suggestedColor).css(),
+        hsl: chroma(suggestedColor).css('hsl'),
+        distance: distance.distance,
+      });
+    });
+    this.cdr.detectChanges();
+    this.updateBackground(color, this.resultColors);
+  }
+
+  updateBackground(inputColor: string, resultColors: ResultColor[]): void {
+    this.container.nativeElement.style.backgroundColor = chroma(
+      inputColor
+    ).alpha(0.65);
   }
 
   /**
@@ -59,5 +100,12 @@ export class AppComponent implements OnInit {
             valid: false,
           },
         };
+  }
+
+  getColorFormValue(): string {
+    const color = this.colorForm.get('color').value;
+    if (chroma.valid(color)) {
+      return chroma(this.colorForm.get('color').value).hex();
+    }
   }
 }
