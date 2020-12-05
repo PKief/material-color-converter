10079cecb6a8bbb2f4a7d8b745b8eb4e31759461
PKief <philipp.kief@gmx.de>
2020-12-05 22:51:11 +0100

    Update karma coverage reporter

--- karma.conf.js
+++ karma.conf.js
@@ -9,21 +9,13 @@ module.exports = function (config) {
       require("karma-jasmine"),
       require("karma-chrome-launcher"),
       require("karma-jasmine-html-reporter"),
-      require("karma-coverage-istanbul-reporter"),
+      require("karma-coverage"),
       require("@angular-devkit/build-angular/plugins/karma"),
     ],
     client: {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: require("path").join(
-        __dirname,
-        "./coverage/material-color-converter"
-      ),
-      reports: ["html", "lcovonly", "text-summary"],
-      fixWebpackSourcePaths: true,
-    },
-    reporters: ["progress", "kjhtml"],
+    reporters: ["progress", "kjhtml", "coverage"],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
@@ -31,5 +23,9 @@ module.exports = function (config) {
     browsers: ["ChromeHeadless"],
     singleRun: false,
     restartOnFileChange: true,
+    coverageReporter: {
+      type: "html",
+      dir: "coverage/",
+    },
   });
 };
