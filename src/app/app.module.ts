1dbf3e90760eee8ca15e397485d5915da42b3495
Philipp Kief <philipp.kief@gmx.de>
2022-05-23 21:42:57 +0200

    Update input of colors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { IMaskModule } from 'angular-imask';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,6 +16,7 @@ import { SharedModule } from './shared/shared.module';
     BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
+    IMaskModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
