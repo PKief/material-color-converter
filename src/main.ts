22121d3ef1a38f8040214fdb081434eb04497611
Philipp Kief <philipp.kief@gmx.de>
2022-10-21 20:44:12 +0200

    Add vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { createPinia } from "pinia";
 import { createApp } from "vue";
-
 import App from "./App.vue";
-
 import "./assets/main.scss";
+import vuetify from "./plugins/vuetify";
+import { loadFonts } from "./plugins/webfontloader";
+
+loadFonts();
 
 const app = createApp(App);
 
 app.use(createPinia());
+app.use(vuetify);
 
 app.mount("#app");
